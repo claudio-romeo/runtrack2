a92e525856c0795f051a4f184fa30baab0b0a016
romeo <claudio.romeo@laplateforme.io>
2021-11-03 12:51:18 +0100

    fin du projet 1 de la runtrack2

---
+++ jour01/job03/index.php
@@ -0,0 +1,36 @@
+<?php
+
+$var = 6;
+$str = 1.06;
+$frt = 'Je suis le chat';
+$thebool = true;
+?>
+
+
+
+<html>
+<table>
+    <thead>
+        <th>
+            <td colspan="3">type&emsp;nom&emsp;valeur&emsp;</td>
+            <!-- <td>nom</td>
+            <td>valeur</td> -->
+        </th>
+    </thead>
+
+
+<tr>
+<table>
+    <td colspan="3">Int&emsp; var&emsp;<?php echo $var; ?></td></tr>
+    <br>
+    <tr><td colspan="3">float&emsp; str&emsp;<?php echo $str; ?> </td></tr>
+    <tr><td colspan="3">string&emsp; frt&emsp;<?php echo $frt; ?> </td></tr>
+   <tr> <td colspan="3">boolean&emsp; thebool&emsp;<?php echo $thebool; ?> </td></tr>
+   </table>
+   
+ 
+</tr>
+</table>
+</html>
+
+<!-- (booléen, entier, chaîne de caractères, nombre à virgule flottante) -->

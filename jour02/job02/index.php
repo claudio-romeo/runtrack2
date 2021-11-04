36a4c7d0a1291aa705b2ccbb76cc52e66bb4065d
claudio-romeo <91873806+claudio-romeo@users.noreply.github.com>
2021-11-04 19:14:28 +0100

    Add files via upload

---
+++ jour02/job02/index.php
@@ -0,0 +1,27 @@
+<?php 
+
+$i=0;
+
+while ($i<=1337) {
+
+
+if ($i==26 || $i==37 || $i==88 || $i==1111 || $i==1233) {
+    echo "<br/>";
+}
+
+// 3233 ce chiffre n'existe pas dans la ligne de chiffre demandé du coup je suppose a un miss clique et donc fait disparaitre le 1233// 
+
+else {echo "$i"."<br/>";
+    }
+$i++;
+}
+
+// si j'atteint le chiffre 8 ou 13 alors tu dois afficher un br//
+// sinon tu continu d'afficher les chiffres //
+
+
+
+
+
+
+?>
